80ee1e9d474754fdbea2157d1cdeac64941c83b6
Mihail Bobeica <mihailbobeica.07@gmail.com>
2022-01-09 16:48:19 +0100

    controller aggiornato

--- src/main/java/com/twitter/friends/Controller.java
+++ src/main/java/com/twitter/friends/Controller.java
@@ -14,51 +14,66 @@ import com.twitter.models.TwitterUser;
 
 @RestController
 public class Controller {
+	
 	@GetMapping("/stats")	
 	public String stats(@RequestParam(value = "username") String username) {
-		try {
-			TwitterUser tu = Caller.OBJECT_MAPPER.readValue(Caller.jsonTwitterUserDataFromUsername(username), TwitterUser.class);
-			tu.initFriends();
-			ObjectNode root = Caller.OBJECT_MAPPER.createObjectNode();
-			
-			int fsan = tu.friendsFollowersAverageNumber();
-			root.put("friends_followers_average_number", fsan);
-			
-			int fgan = tu.friendsFollowingAverageNumber();
-			root.put("friends_following_average_number", fgan);
-			
-			int twan = tu.friendsTweetsAverageNumber();
-			root.put("friends_tweets_average_number", twan);
-			
-			float prwd = tu.friendsPercentageWithDescription();
-			root.put("friends_percentage_with_description", prwd);
-			
-			String jsonString = Caller.OBJECT_MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(root);
-			return jsonString;
-		} catch (JsonProcessingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return "some error";
+		String jsonTwitterUserData = Caller.jsonTwitterUserDataFromUsername(username);
+		if (Caller.isNotNullOrEmptyString(jsonTwitterUserData)) {
+			try {
+				TwitterUser tu = Caller.OBJECT_MAPPER.readValue(jsonTwitterUserData, TwitterUser.class);
+				tu.initFriends();
+				
+				ObjectNode root = Caller.OBJECT_MAPPER.createObjectNode();
+				
+				int fsan = tu.friendsFollowersAverageNumber();
+				root.put("friends_followers_average_number", fsan);
+				
+				int fgan = tu.friendsFollowingAverageNumber();
+				root.put("friends_following_average_number", fgan);
+				
+				int twan = tu.friendsTweetsAverageNumber();
+				root.put("friends_tweets_average_number", twan);
+				
+				float prwd = tu.friendsPercentageWithDescription();
+				root.put("friends_percentage_with_description", prwd);
+				
+				String jsonString = Caller.OBJECT_MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(root);
+				return jsonString;
+			} catch (JsonProcessingException e) {
+				// TODO Auto-generated catch block
+				// e.printStackTrace();
+				return "Controller stats try catch 0";
+			}
+		} else {
+			// TODO some error message
+			return "Controller stats if else 0";
 		}
+		 
 	}
 	
-	@GetMapping("/filters")	
-	public String filters(@RequestParam(value = "username") String username, @RequestParam(value = "word") String word) {
-		try {
-			TwitterUser tu = Caller.OBJECT_MAPPER.readValue(Caller.jsonTwitterUserDataFromUsername(username), TwitterUser.class);
-			tu.initFriends();
-			ArrayNode root = Caller.OBJECT_MAPPER.createArrayNode();
-			
-			for (BasicUser bu : tu.filterBasedOnFriendsDescription(word)) {
-				root.add(Caller.OBJECT_MAPPER.convertValue(bu, JsonNode.class));
+	@GetMapping("/filter")	
+	public String filter(@RequestParam(value = "username") String username, @RequestParam(value = "word") String word) {
+		String jsonTwitterUserData = Caller.jsonTwitterUserDataFromUsername(username);
+		if (Caller.isNotNullOrEmptyString(jsonTwitterUserData)) {
+			try {
+				TwitterUser tu = Caller.OBJECT_MAPPER.readValue(jsonTwitterUserData, TwitterUser.class);
+				tu.initFriends();
+				
+				ArrayNode root = Caller.OBJECT_MAPPER.createArrayNode();
+				for (BasicUser bu : tu.filterBasedOnFriendsDescription(word)) {
+					root.add(Caller.OBJECT_MAPPER.convertValue(bu, JsonNode.class));
+				}
+				
+				String jsonString = Caller.OBJECT_MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(root);
+				return jsonString;
+			} catch (JsonProcessingException e) {
+				// TODO Auto-generated catch block
+				// e.printStackTrace();
+				return "Controller filter try catch 0";
 			}
-			
-			String jsonString = Caller.OBJECT_MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(root);
-			return jsonString;
-		} catch (JsonProcessingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return "some error";
+		} else {
+			// TODO some error message
+			return "Controller filters if else 0";
 		}
 	}
 }
