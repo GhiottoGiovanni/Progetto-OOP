c609f1aca21ef06b1483596dc455e6b713068a35
Mihail Bobeica <mihailbobeica.07@gmail.com>
2022-01-09 17:50:22 +0100

    salvataggio in locale
    il salvataggio dei dati degli amici in locale permette di fare un numero di chiamate non limitato e in maniera più rapida

--- src/main/java/com/twitter/models/TwitterUser.java
+++ src/main/java/com/twitter/models/TwitterUser.java
@@ -1,8 +1,14 @@
 package com.twitter.models;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.exc.StreamWriteException;
+import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.twitter.view.Filters;
 import com.twitter.view.Statistics;
@@ -10,6 +16,10 @@ import com.twitter.view.Statistics;
 import com.twitter.controller.Caller;
 
 public class TwitterUser extends User implements Filters, Statistics{
+	
+	// private static String USERS_STORAGE_PATH = "./src/main/resources/data/usernames/";
+	private static String FRIENDS_STORAGE_PATH = "./src/main/resources/data/friends/";
+	
 	private ArrayList<User>	friends = new ArrayList<User>();
 	private int friends_count;
 	
@@ -26,8 +36,36 @@ public class TwitterUser extends User implements Filters, Statistics{
 	private void setFriends_count(int friends_count) {
 		this.friends_count = friends_count;
 	}
-
+	
+	private String getFriendsFileName() {
+		return FRIENDS_STORAGE_PATH + this.getUsername() + ".json";
+	}
+	
 	public void initFriends() {
+		// TODO add an auto delete data when closed option
+		if (new File(getFriendsFileName()).exists()) {
+			initFriendsFromLocal();
+		} else {
+			initFriendsFromRequest();
+		}
+	}
+	
+	private void initFriendsFromLocal() {
+		try {
+			User[] friends = Caller.OBJECT_MAPPER.readValue(Paths.get(getFriendsFileName()).toFile(), User[].class);
+			Collections.addAll(this.friends, friends);
+			this.setFriends_count(this.friends.size());
+			System.out.println("dati presi e caricati correttamente");
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			// e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			// e.printStackTrace();
+		}
+	}
+
+	private void initFriendsFromRequest() {
 		String nextToken = null;
 		do {
 			String friendsData = Caller.followingDataFromUsername(this.getUsername(), nextToken);
@@ -46,7 +84,21 @@ public class TwitterUser extends User implements Filters, Statistics{
 				e.printStackTrace();
 			}
 		} while (nextToken != null && !nextToken.isBlank());
-		this.setFriends_count(friends.size());
+		this.setFriends_count(this.friends.size());
+		
+		// store friends data
+		try {
+			Caller.OBJECT_MAPPER.writeValue(Paths.get(getFriendsFileName()).toFile(), this.friends);
+		} catch (StreamWriteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (DatabindException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	// statistics START
