e7cd6e0512948de2db34deb651900dd93a625163
Mihail Bobeica <mihailbobeica.07@gmail.com>
2022-01-11 11:34:47 +0100

    aggiunti metadata al filtro

--- src/main/java/com/twitter/filters/friends/FilterByWordInDescription.java
+++ src/main/java/com/twitter/filters/friends/FilterByWordInDescription.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.twitter.controller.Caller;
 import com.twitter.models.BasicUser;
 import com.twitter.models.User;
@@ -16,14 +17,21 @@ public class FilterByWordInDescription extends Filter{
 	}
 	
 	public String filteredData(String word) {
-		ArrayNode root = Caller.OBJECT_MAPPER.createArrayNode();
+		ObjectNode root = Caller.OBJECT_MAPPER.createObjectNode();
+		ArrayNode data = Caller.OBJECT_MAPPER.createArrayNode();
+		ObjectNode meta = Caller.OBJECT_MAPPER.createObjectNode();
+		int result_count = 0;
 		for (User u : getFriends()) {
 			// avoids case sensitivity
 			if (u.getDescription().toLowerCase().contains(word.toLowerCase())) {
 				BasicUser bu = new BasicUser(u.getId(), u.getName(), u.getUsername());
-				root.add(Caller.OBJECT_MAPPER.convertValue(bu, JsonNode.class));
+				data.add(Caller.OBJECT_MAPPER.convertValue(bu, JsonNode.class));
+				result_count++;
 			}
 		}
+		meta.put("result_count", result_count);
+		root.set("data", data);
+		root.set("meta", meta);
 		try {
 			return Caller.OBJECT_MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(root);
 		} catch (JsonProcessingException e) {
