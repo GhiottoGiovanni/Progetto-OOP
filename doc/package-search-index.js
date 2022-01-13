87efa19161d16d69bdd8acb0edbfcbff524b2ce2
Giovanni <S1097754@univpm.studenti.it>
2022-01-13 18:11:32 +0100

    files javadoc

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.twitter.statistics.friends"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.twitter.APIcaller"},{"l":"com.twitter.filters.friends"},{"l":"com.twitter.models"},{"l":"com.twitter.statistics.friends"}];updateSearchResults();
