45953f703d2a51900d2d67291d90b706018b6e9e
Giovanni <S1097754@univpm.studenti.it>
2022-01-14 17:39:37 +0100

    javadoc eccezioni

--- src/main/java/com/twitter/exceptions/NotExistingAccountException.java
+++ src/main/java/com/twitter/exceptions/NotExistingAccountException.java
@@ -1,6 +1,15 @@
 package com.twitter.exceptions;
 
+/**
+ * <b>Eccezione</b> che avvisa quando non esiste l'utente nell'API Twitter.
+ * @author Giovanni Ghiotto
+ * @author Mihail Bobeica
+ *@version 1.0
+ */
 public class NotExistingAccountException extends Exception{
+	/**
+	 * @param username Nome identificativo di un utente.
+	 */
 	public NotExistingAccountException(String username){
 		super("Non esiste nessun account Twitter con il seguente nome identificativo: " + username);
 	}
